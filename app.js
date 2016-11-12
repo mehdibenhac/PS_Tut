bc00c4e72e822c2929bc5edc25820cbfb6d49dc1
Ghuen Lee <ghuenlee@gmail.com>
2016-11-12 20:36:46 +0100

    adding passport ...

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Imports
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var nav = [{
     link: '/Books',
@@ -10,27 +14,32 @@ var nav = [{
     text: 'Authors'
 }];
 
+// Routes
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
 var authRouter = require('./src/routes/authRoutes')(nav);
-//var authorRouter = require('./src/routes/authorRoutes')(nav);
 
-var port = process.env.PORT || 5000;
 
+// Middleware
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'library'
+}));
+require('./src/config/passport.js')(app);
 
+// Templating Engine
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+// Routing
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
 app.use('/Auth', authRouter);
-//app.use('/Authors', authorRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -44,6 +53,9 @@ app.get('/', function (req, res) {
         }]
     });
 });
+
+// Port listening
+var port = process.env.PORT || 5000;
 
 app.listen(port, function (err) {
     console.log('Server started on port: ' + port);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,7 +7,15 @@ var router = function (nav) {
     authRouter.route('/signup')
         .post(function (req, res) {
             console.log(req.body);
+            req.login(req.body, function () {
+                res.redirect('/auth/profile');
+            });
         });
+    authRouter.route('/profile')
+        .get(function (req, res) {
+            res.json(req.user);
+        });
+
     return authRouter;
 };
 
